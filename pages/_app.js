3591d22b7e9e8b41db81216e394ca02482d7c520
kakoibi-app <kakoibi.official@gmail.com>
2026-06-09 09:29:51 +0900

    fix currentPos handling

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
 import "../styles.css";
 import Script from "next/script";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      {/* ✅ PWA / ホーム画面用設定 */}
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+
+        {/* iOS向け設定 */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name="apple-mobile-web-app-title" content="CalmFire" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+      </Head>
+
+      {/* ✅ Google AdSense */}
       <Script
         async
         strategy="afterInteractive"
